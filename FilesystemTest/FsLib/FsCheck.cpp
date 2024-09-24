35c8313b1ccc54a0debd5d2e82c8945c6d2a49aa
Jim Borden <borrrden@gmail.com>
2024-09-24 16:38:49 +0900

    Add CMake project

--- FilesystemTest/FsLib/FsCheck.cpp
+++ FilesystemTest/FsLib/FsCheck.cpp
@@ -1,5 +1,12 @@
 #include "FsCheck.h"
 #include "FsLogger.h"
+#if !defined(CMAKE)
+#ifdef __clang__
+#define HAS_BUILTIN_TRAP 1
+#elif defined(_MSC_VER)
+#define HAS_DEBUG_BREAK 1
+#endif
+#endif
 
 void CheckImplementer::Check(const char* Message)
 {
@@ -7,5 +14,10 @@ void CheckImplementer::Check(const char* Message)
 	FsLogger::Log(FilesystemLogType::Fatal, Message);
 
 	// Die
+
+#if HAS_BUILTIN_TRAP
+	__builtin_trap();
+#else
 	*(char*)0x00 = 3;
+#endif
 }
---
+++ cmake/check_builtin.c
@@ -0,0 +1,7 @@
+#ifndef BUILTIN_FUNC
+#error Must define BUILTIN_FUNC for this to work
+#endif
+
+void main() {
+    BUILTIN_FUNC();
+}
