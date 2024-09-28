6032f1b6da595edce87df447e9f2642caf8c17d3
Jim Borden <borrrden@gmail.com>
2024-09-29 08:13:41 +0900

    Update CMake to find and use Dokan

---
+++ cmake/find_dokan.cmake
@@ -0,0 +1,47 @@
+MACRO(ls result curdir)
+  FILE(GLOB children ${curdir}/*)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${child})
+      LIST(APPEND dirlist ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()
+
+macro(find_dokan)
+    if(DEFINED DOKAN_LIB)
+        if(NOT EXISTS ${DOKAN_LIB})
+            message(FATAL_ERROR "User-specified Dokan lib does not exist: ${DOKAN_LIB}")
+        endif()
+
+        message(STATUS "Using user-specified Dokan lib at ${DOKAN_LIB}")
+    else()
+        message(STATUS "Looking for Dokan libs")
+        ls(dokan_folders "C:/Program Files/Dokan")
+        list(LENGTH dokan_folders num_results)
+        if(num_results EQUAL 0)
+            message(WARNING "No Dokan installation auto detected, unless the libs are in your PATH this will fail")
+            find_library(
+                DOKAN_LIB
+                dokan2 REQUIRED
+            )
+        else()
+            math(EXPR last_index "${num_results} - 1")
+            list(GET dokan_folders ${last_index} dokan_hint_folder)
+            if(${num_results} GREATER 1)
+                message(WARNING "Multiple Dokan installations found, using the one that sorted last: ${dokan_hint_folder}")
+                message("Set the DOKAN_LIB variable to the path to dokan2.lib in order to silence this warning")
+            else(
+                message(STATUS "Found Dokan lib at ${dokan_hint_folder}")
+            )
+            endif()
+
+            find_library(
+                DOKAN_LIB
+                dokan2 REQUIRED
+                HINTS ${dokan_hint_folder}
+            )
+        endif()
+    endif()
+endmacro()
